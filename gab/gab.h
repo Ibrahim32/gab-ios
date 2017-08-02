8b100725267f6b56e78e9d21ed53b11bac386919
khanxc <khan.kmk@gmail.com>
2017-08-03 00:23:16 +0530

    Initial Commit

---
+++ gab/gab.h
@@ -0,0 +1,19 @@
+//
+//  gab.h
+//  gab
+//
+//  Created by Khan on 03/08/17.
+//  Copyright © 2017 com.appyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gab.
+FOUNDATION_EXPORT double gabVersionNumber;
+
+//! Project version string for gab.
+FOUNDATION_EXPORT const unsigned char gabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gab/PublicHeader.h>
+
+
